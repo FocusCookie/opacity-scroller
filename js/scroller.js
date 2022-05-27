5d9fe41761af9336f63a8b0d872fc4318ee80b1e
Stephan Lüddemann <stephan@MBP-Xcnt.fritz.box>
2022-05-27 21:06:19 +0200

    Working prototype. Some fixes needed

--- js/scroller.js
+++ js/scroller.js
@@ -1,19 +1,86 @@
 const osWrappers = document.getElementsByClassName("os__wrapper");
 
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+const isInViewport = function (elem) {
+  if (!elem) return false;
+
+  const bounding = elem.getBoundingClientRect();
+
+  const height = window.innerHeight || document.documentElement.clientHeight;
+
+  const topBoundry = height * 0.25;
+  const bottomBoundry = height * 0.75;
+
+  const middleOnScreen = bounding.top + bounding.height / 2;
+
+  /* return bounding.top >= 0 && bounding.bottom <= height; */
+
+  return middleOnScreen >= topBoundry && middleOnScreen <= bottomBoundry;
+};
+
 if (osWrappers.length > 0) {
-  wrapper = osWrappers[0];
+  Object.keys(osWrappers).forEach((wrapperKey) => {
+    const wrapper = osWrappers[wrapperKey];
+    const textContainerKey = getFirstKeyOfChildrenWithClass(
+      wrapper,
+      "os__text-container"
+    );
+    const textContainer = wrapper.children[textContainerKey];
 
-  // get all p in the text-container
-  const osParagraphs = document.getElementsByClassName("os__text-item");
+    Object.keys(textContainer.children).forEach((pKey) => {
+      const uuid = uuidv4();
 
-  // add as many ghost boxes to the os__wrapper as ps are in text-container
-  Object.keys(osParagraphs).forEach((pKey) => {
-    const ghost = document.createElement("div");
-    ghost.classList.add("os__ghost");
-    ghost.setAttribute("data-parent", `os-${pKey}`);
-    wrapper.append(ghost);
+      const ghost = document.createElement("div");
+      ghost.classList.add("os__ghost");
+      ghost.setAttribute("data-ghost-uuid", uuid);
+      wrapper.append(ghost);
+
+      textContainer.children[pKey].setAttribute("data-p-uuid", uuid);
+    });
   });
 
   // add ad eventlistener for scroll
   //  if the n element is in the n ghost element set the opacity to 100 if not set opactiy to 0.2
+}
+
+const ghosts = document.getElementsByClassName("os__ghost");
+
+window.addEventListener(
+  "scroll",
+  function (event) {
+    for (ghost of ghosts) {
+      const uuid = ghost.dataset.ghostUuid;
+      const linkedParagraph = document.querySelector(`[data-p-uuid="${uuid}"]`);
+
+      if (isInViewport(ghost)) {
+        linkedParagraph.classList.add("os__text-item--on");
+      } else {
+        linkedParagraph.classList.remove("os__text-item--on");
+      }
+    }
+  },
+  false
+);
+
+function getFirstKeyOfChildrenWithClass(element, className) {
+  if (!element) return 1;
+
+  if (element.childElementCount === 0) return -1;
+
+  const key = Object.keys(element.children).findIndex((key) => {
+    element.children[key].children;
+  });
+
+  const childrens = Object.keys(element.children).map(
+    (key) => element.children[key]
+  );
+
+  return childrens.findIndex((child) => child.classList.contains(className));
 }
